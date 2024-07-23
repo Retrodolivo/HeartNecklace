a2265183357aa4491be363e9683a5e11439a1f43
retroboy <naskin.iv@gmail.com>
2024-07-23 22:39:46 +0300

    Implemented heatbeat led pattern via external interrupt

--- HeartNecklace/main.asm
+++ HeartNecklace/main.asm
@@ -1,14 +1,16 @@
 .include "common_macros.asm"
 .list
 
-.equ	LED_PIN = pinb0
+.equ	LED_PIN = pinb1
 .equ	BTN_PIN = pinb2
-/*(1_600_000 / 1024) = 1953 - timer ticks per second
-  (1 / 1953) = 1ms - per tick
+/*(1_000_000 / 1024) = 976 - timer ticks per second
+  (1 / 976) = 1ms - per tick
   (x / 1ms) = TIMER_THRESHOLD, x - delay in ms
   x = 1ms * TIMER_THRESHOLD
 */
 .equ	TIMER_THRESHOLD = 150
+.equ	PWM_MAX_VALUE = 150
+
 .def	temp = r16
 .def	innerDelay = r17
 .def	outerDelay = r18
@@ -41,30 +43,67 @@ reset:
 ; Delay timer initialization
 		ldi temp, (1<<CS00) | (0<<CS01) | (1<<CS02)
 		out TCCR0B, temp
+; PWM timer initialization
+		; Synchronous mode
+		ldi temp, (0<<PCKE)
+		out PLLCSR, temp
+		; PWM mode A enable, Clear OC1A pin on compare, 8 prescaler
+		ldi temp, (1<<PWM1A) | (1<<COM1A1) | (0<<CS13) | (1<<CS12) | (0<<CS11) | (0<<CS10)
+		out TCCR1, temp
+		; Compare register value
+		ldi temp, PWM_MAX_VALUE
+		out OCR1C, temp
+
+		ldi temp, 0
+		out OCR1A, temp
+
 ; External interrupts initialization
 		ldi temp, (0<<ISC00) | (1<<ISC01) ; falling edge
 		out MCUCR, temp
 		ldi temp, (1<<INT0)
 		out GIMSK, temp
 		sei
+		rjmp main
 
 main:
-/* Light LED via button
-buttonCheck:	
-		sbic PINB, BTN_PIN
-		rjmp buttonCheck
-led:	
-		sbi PORTB, LED_PIN
-		rcall delay
-		sbis PINB, BTN_PIN
-		rjmp led
-		cbi PORTB, LED_PIN
-		rjmp buttonCheck
-*/
+		rjmp main
 
-/* Blinking LED */
-loop:
-		rjmp loop
+heartbeat:
+		ldi temp, 0
+heartbeatSmallUp:
+		; wait before inc brightness
+		rcall delay
+		; inc brightness
+		inc temp
+		out OCR1A, temp
+		; check if enough brightness
+		cpi temp, PWM_MAX_VALUE * 0.5
+		brne heartbeatSmallUp
+heartbeatSmallDown:
+		; wait before dec brightness
+		rcall delay
+		dec temp
+		out OCR1A, temp
+		; check if enough brightness
+		cpi temp, 3
+		brne heartbeatSmallDown
+heartbeatBigUp:
+		; wait before inc brightness
+		rcall delay
+		; inc brightness
+		inc temp
+		out OCR1A, temp
+		; check if enough brightness
+		cpi temp, PWM_MAX_VALUE * 0.8
+		brne heartbeatBigUp
+heartbeatBigDown:
+		; wait before dec brightness
+		rcall delay
+		dec temp
+		out OCR1A, temp
+		; check if enough brightness
+		cpi temp, 0
+		brne heartbeatBigDown
 
 timerDelay:
 		push temp
@@ -80,7 +119,7 @@ m1:
 delay:
 		ldi innerDelay, 255
 outerLoop:
-		ldi outerDelay, 255
+		ldi outerDelay, 5
 		dec innerDelay
 		brne innerLoop
 		ret
@@ -91,10 +130,9 @@ innerLoop:
 
 
 int0_ISR:
-		sbi PORTB, LED_PIN
-		rcall delay
-		cbi PORTB, LED_PIN
-		rcall delay
+		cli
+		rcall heartbeat
+		sei
 		reti
 
 ;EEPROM ==========================================
